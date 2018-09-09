f8b89eba40d5c9c656b47263a3388200af26cb20
James Layton <jameshanxinsun@gmail.com>
2018-09-09 14:38:04 -0400

    Update Contents.swift

--- Longest_Substring_Withought_Repeating_Characters.playground/Contents.swift
+++ Longest_Substring_Withought_Repeating_Characters.playground/Contents.swift
@@ -1,45 +1,68 @@
 
 /*
  Problem: Given a string, find the length of the longest substring without repeating characters.
- Example:
-    Given "abcabcbb", the answer is "abc", which the length is 3
-    Given "bbbbb", the answer is "b", with the length of 1
-    Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, 
-    "pwke" is a subsequence and not a substring
+ Examples:
+ Given: "abcabcbb", the answer is "abc", which the length is 3
+ Given: "bbbbb", the answer is "b", with the length of 1
+ Given: "pwwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring,
+ "pwke" is a subsequence and not a substring
  */
 
-func lengthOfLongestSubstring(words: String) -> Int {
+func findLongestSubstring(_ word: String) -> Int {
     
-    if words.count == 0 {
+    if word.count == 0 {
         return 0
     }
     
     var charTable = [Character:Int]()
     var maxLength: Int = 0
-    var lastDuplicateIndex: Int = -1 // Last index of the samiliar character
+    var lastDuplicateIndex: Int = -1
     
-    let charArray = Array(words) // Convert the string to character array
+    let charArray = Array(word)
     
     for i in 0..<charArray.count {
         
-        if let lastCharIndex = charTable[charArray[i]], lastDuplicateIndex < lastCharIndex { 
-         // Check if character already exist in dictionary and check if lastDuplicateIndex is smaller than the lastChartIndex from dictionary
-            lastDuplicateIndex = lastCharIndex // Update the lastDuplicateIndex
+        let currentChar = charArray[i]
+        if let charIndex = charTable[currentChar], lastDuplicateIndex < charIndex {
+            lastDuplicateIndex = charIndex
         }
-        maxLength = max(maxLength, i-lastDuplicateIndex) // Find the greatest number
-        charTable[charArray[i]] = i // Add the existing to the dictionary
-    
+        let currentLength = i-lastDuplicateIndex
+        maxLength = max(maxLength, currentLength)
+        charTable[currentChar] = i
     }
     
     return maxLength
 }
 
-/* >>>>>>>>>>> TEST <<<<<<<<<<< */
+/* >>>>> TRACKING <<<<<
+ Example: "abcabcbb"
+ 
+ ------------------
+ NOTE!!: currentLength is alwasy compare to previous maxLength
+ current char            :  a ->  b ->  c -> a -> b -> c -> b -> b
+ current Index           :  0 ->  1 ->  2 -> 3 -> 4 -> 5 -> 6 -> 7
+ lastDuplicateIndex      : -1 -> -1 -> -1 -> 0 -> 1 -> 2 -> 4 -> 6
+ currentLength           :  1 ->  2 ->  3 -> 3 -> 3 -> 3 -> 2 -> 1
+ maxLength         :  0 ->  1 ->  2 ->  3 -> 3 -> 3 -> 3 -> 3 -> 3
+ ------------------
+ CharTable:
+ [a:0] ->
+ [a:0, b:1] ->
+ [a:0, b:1, c:2] ->
+ [a:0, b:1, c:2, a:3] ->
+ [a:0, b:1, c:2, a:3, b:4] ->
+ [a:0, b:1, c:2, a:3, b:4, c:5] ->
+ [a:0, b:1, c:2, a:3, b:4, c:5, b:6] ->
+ [a:0, b:1, c:2, a:3, b:4, c:5, b:6, b:7]
+ ------------------
+ 
+ */
 
-let testWords1 = "abcabcbb"
-let testWords2 = "bbbbb"
-let testWords3 = "pwwkew"
+/* >>>>> TEST <<<<< */
+let testString1 = "abcabcbb"
+let testString2 = "bbbbb"
+let testString3 = "pwwkew"
 
-print("Result is ", lengthOfLongestSubstring(words: testWords1))
-print("Result is ", lengthOfLongestSubstring(words: testWords2))
-print("Result is ", lengthOfLongestSubstring(words: testWords3))
+print("Result is ", findLongestSubstring(testString1))
+print("Result is ", findLongestSubstring(testString2))
+print("Result is ", findLongestSubstring(testString3))
